2e093e26e63328d2fd3da4a729130f1eac6dfe30
AnilShebin <anilshebin@gmail.com>
2024-08-04 19:24:27 +0530

    Update Navbar
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import { CgMenuBoxed } from "react-icons/cg";
+import { CgMenuRight } from "react-icons/cg";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -17,20 +17,18 @@ const Navbar: React.FC = () => {
 
       <nav className={`navbar ${isOpen ? "active" : ""}`}>
         <div className={`nav-link ${isOpen ? "show" : ""}`}>
-          {/* <a href="#">Punch</a> */}
-          <a href="#">Home</a>
-          <a href="#">Services</a>
-          <a href="#">Industry</a>
-          <a href="#">Technology</a>
-          <a href="#">Clients</a>
-          <a href="#">About Us</a>
-          <button className="login-button" onClick={() => window.location.href='#'}>
-            Contact Us
+          <a href="/">Services</a>
+          <a href="/">Industry</a>
+          <a href="/">Technology</a>
+          <a href="/">Clients</a>
+          <a href="/">About Us</a>
+          <button className="login-button" onClick={() => window.location.href='/'}>
+          Contact Us
           </button>
         </div>
       </nav>
       <div className="icon-menu" onClick={toggleMenu}>
-        <CgMenuBoxed />
+        <CgMenuRight />
       </div>
     </header>
   );
